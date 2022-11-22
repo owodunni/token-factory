69a8fdfdf762c87355deb10d868448bfe1e2e5e2
Alexander Poole <alex.o.poole@gmail.com>
2022-11-22 16:51:55 +0100

    fix(app): add custom error page

---
+++ packages/app/src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<article class="prose px-8 py-16">
+<h1>{$page.status}: {$page.error?.message}</h1>
+  <h3>Did you get lost? <a class="text-primary" href="/">Head home!</a></h3>
+</article>
