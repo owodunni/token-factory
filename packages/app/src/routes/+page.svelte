b22def71a8bc10629c3a3e808f9225b5f83567f8
Alexander Poole <alex.o.poole@gmail.com>
2022-11-22 16:03:29 +0100

    feat(app): basic layout of Token Factory

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -1,13 +1,10 @@
-<p>Here's some DaisyUI buttons, yummy!</p>
-
-<button class="btn">neutral</button>
-<button class="btn btn-primary">primary</button>
-<button class="btn btn-secondary">secondary</button>
-<button class="btn btn-accent">accent</button>
-<button class="btn btn-ghost">ghost</button>
-<button class="btn btn-link">link</button>
-
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
-  documentation
-</p>
+<div class="px-8 py-16">
+  <article class="prose">
+    <h1>Token <span class="text-primary">Factory</span></h1>
+    <h3>An educational tool for learning about DeFi.</h3>
+  </article>
+  <div class="my-4 flex space-x-4">
+    <button class="btn">About</button>
+    <button class="btn btn-primary">Mint</button>
+  </div>
+</div>
