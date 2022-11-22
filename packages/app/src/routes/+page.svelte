84d433b13389260aed57c9b32d82432145ccd7e6
Alexander Poole <alex.o.poole@gmail.com>
2022-11-22 11:18:49 +0100

    chore(deps): add daisyUI

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Here's some DaisyUI buttons, yummy!</p>
+
+<button class="btn">neutral</button>
+<button class="btn btn-primary">primary</button>
+<button class="btn btn-secondary">secondary</button>
+<button class="btn btn-accent">accent</button>
+<button class="btn btn-ghost">ghost</button>
+<button class="btn btn-link">link</button>
+
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+  documentation
+</p>
--- packages/app/tailwind.config.cjs
+++ packages/app/tailwind.config.cjs
@@ -1,8 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
-	plugins: []
+	plugins: [require("daisyui")],
+  daisyui: {
+    darkTheme: "dark"
+  }
 };
