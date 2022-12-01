59e65913e7b385e6a0e04f09dc386db0c906b373
Alexander Poole <alex.o.poole@gmail.com>
2022-12-01 16:55:26 +0100

    fix(layout): fix items not being centerd and broken background

--- packages/app/src/lib/components/PostList.svelte
+++ packages/app/src/lib/components/PostList.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import type{PostMetadata} from "$lib/api";
+    import type {PostMetadata} from "$lib/api";
 
     export let posts: PostMetadata[];
     export let category: string;
@@ -7,13 +7,17 @@
 <article class="prose">
     <h3>{category}</h3>
 </article>
-<section class="flex flex-nowrap overflow-x-auto max-w-sm md:max-w-full snap-mandatory gap-5 no-scrollbar py-4 md:grid md:grid-cols-2 lg:grid-cols-3">
-    {#each posts as post}
-        <a href="/blog/{post.metadata.post}" class="rounded-md flex-none snap-always snap-center rounded-md bg-base-100 w-80 h-48 shadow-lg p-4 hover:scale-[1.03]">
-            <article class="prose">
-                <h2>{post.metadata.title}</h2>
-                <p class="line-clamp-3">{post.metadata.excerpt}</p>
-            </article>
-        </a>
-    {/each}
-</section>
+<div class="flex place-items-center">
+    <section
+            class="flex flex-nowrap overflow-x-auto snap-mandatory gap-5 no-scrollbar py-4 sm:grid sm:grid-cols-2 lg:grid-cols-3">
+        {#each posts as post}
+            <a href="/blog/{post.metadata.post}"
+               class="rounded-md flex-none snap-always snap-center rounded-md bg-base-100 w-60 h-48 shadow-lg p-4 hover:scale-[1.03]">
+                <article class="prose">
+                    <h2>{post.metadata.title}</h2>
+                    <p class="line-clamp-3">{post.metadata.excerpt}</p>
+                </article>
+            </a>
+        {/each}
+    </section>
+</div>
