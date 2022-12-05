91081bb0dbc6738e864e407aca97dcad1d732ca7
Alexander Poole <alex.o.poole@gmail.com>
2022-12-05 06:57:03 +0100

    chore(design system): create reusable NavbarButton

---
+++ packages/app/src/lib/components/NavbarButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let size: "sm" | "variable";
+
+
+    let classes = size === "sm" ? "btn-circle" : "";
+</script>
+
+<button class="btn btn-ghost btn-sm bg-base-300 {classes}" >
+    <slot/>
+</button>
