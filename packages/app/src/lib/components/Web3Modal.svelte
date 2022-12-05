508a426a9be73013b290680c526b4ef24dd45c93
Alexander Poole <alex.o.poole@gmail.com>
2022-12-05 06:57:32 +0100

    chore(Web3Modal): trigger input programatically instead of labels

--- packages/app/src/lib/components/Web3Modal.svelte
+++ packages/app/src/lib/components/Web3Modal.svelte
@@ -1,28 +1,38 @@
 <script lang="ts">
     import IconWallet from "$lib/components/icons/IconWallet.svelte";
     import {providerState, disconnect, connect} from "$lib/provider";
+
+    let modalCheckbox: HTMLInputElement;
+    const toggleModal = () => {
+        modalCheckbox.checked = !modalCheckbox.checked;
+    };
+
+    const connectAndToggle = () => {
+        connect();
+        toggleModal();
+    };
 </script>
 
 {#if $providerState.type === "disconnected"}
-<label class="btn btn-ghost btn-sm bg-base-300 hidden sm:block" for="web3-modal">
+<button class="btn btn-ghost btn-sm bg-base-300 hidden sm:block" on:click={toggleModal}>
     <div class="flex flex-row items-center space-x-2">
         <IconWallet/>
         <span class="p-2">Connect</span>
     </div>
-</label>
-<label class="btn btn-ghost btn-sm bg-base-300 sm:hidden btn-circle" for="web3-modal" >
+</button>
+<button class="btn btn-ghost btn-sm bg-base-300 sm:hidden btn-circle" on:click={toggleModal} >
     <div class="flex flex-row items-center justify-evenly space-x-2">
         <IconWallet/>
     </div>
-</label>
+</button>
 {:else if $providerState.type === "connected"}
     <button class="btn btn-ghost btn-sm bg-base-300 hidden sm:block p-2" on:click={disconnect}>{$providerState.selectedAddress}</button>
 {/if}
 
-<input type="checkbox" id="web3-modal" class="modal-toggle" />
+<input type="checkbox" id="web3-modal" class="modal-toggle" bind:this={modalCheckbox} />
 <label for="web3-modal" class="modal modal-bottom sm:modal-middle cursor-pointer">
     <div class="modal-box relative">
-        <label class="btn btn-ghost btn-sm bg-base-300 hidden sm:block p-2" for="web3-modal" on:click={connect} on:keypress={connect}>Connect</label>
+        <button class="btn btn-ghost btn-sm bg-base-300 hidden sm:block p-2" on:click={connectAndToggle}>Connect</button>
     </div>
 </label>
 
