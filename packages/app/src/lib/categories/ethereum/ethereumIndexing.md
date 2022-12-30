3ee66973f1956835e44a4244d1198b892550c6db
Alexander Poole <alex.o.poole@gmail.com>
2022-12-30 07:44:34 +0100

    feat(articles): remove dummy articles

--- packages/app/src/lib/categories/ethereum/ethereumIndexing.md
+++
@@ -1,5 +0,0 @@
----
-title: 'How to do Smart Contracts indexing'
-excerpt: 'Indexing smart contracts using the blockchain transaction history and bloom filters.'
-index: 0
----
--- packages/app/src/lib/categories/ethereum/index.ts
+++
@@ -1,6 +0,0 @@
-import type { Category } from '../../api';
-
-export const category: Category = {
-  excerpt: 'Understand the mechanics of Ethereum and the blockchain.',
-  title: 'Ethereum'
-};
