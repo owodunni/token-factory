e3f32a1f130125e61c42700506c7c220faa82440
Alexander Poole <alex.o.poole@gmail.com>
2023-01-01 16:12:16 +0100

    chore(buttons): remove connect wallet buttons
    User testing showed that the connect wallet buttons atracted to much
    attention from the users making them confused regarding what actions
    where expected on the home screen.

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
   import ArticlesWidget from '$lib/components/ArticlesWidget.svelte';
-
-  import { showWeb3Modal, providerState } from '$lib/provider';
-
-  const toggleModal = () => {
-    showWeb3Modal.update((v) => !v);
-  };
 </script>
 
 <div class="px-2">
@@ -13,16 +7,7 @@
     <article class="lg:max-w-2xl prose text-center prose-sm">
       <h1>Web3 <span class="text-primary">Factory</span></h1>
       <h2>Articles on software, blockchains and web3.</h2>
-      {#if $providerState.type === 'disconnected'}
-        <p>Many of the articles are interactive and require a web3 wallet.</p>
-      {/if}
     </article>
-    {#if $providerState.type === 'disconnected'}
-      <div class="my-4 flex gap-4 justify-center">
-        <button class="btn btn-primary normal-case" on:click={toggleModal}>Connect Wallet</button>
-        <a class="btn normal-case" href="/articles/web3/setup">Setup Wallet</a>
-      </div>
-    {/if}
     <ArticlesWidget />
   </div>
 </div>
