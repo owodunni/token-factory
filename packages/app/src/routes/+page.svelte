77220bb3e528598b16c8bf185ab97a89d5331be8
Alexander Poole <alex.o.poole@gmail.com>
2023-01-08 13:50:48 +0100

    fix(title): title was displayed on three lines, not two

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -5,7 +5,8 @@
 <div class="flex flex-col">
   <article class="prose prose-sm self-center">
     <h1 class="m-0">Web3 <span class="text-primary">Factory</span></h1>
-    <h3 class="m-0 max-w-[200px]">Articles on software, blockchains and web3.</h3>
+    <h3 class="m-0">Articles on software,</h3>
+    <h3 class="m-0">blockchains and web3.</h3>
   </article>
   <ArticlesWidget />
 </div>
